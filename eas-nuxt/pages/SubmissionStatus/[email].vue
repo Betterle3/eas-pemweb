4ef5972c54a64811d445a55e4463a8e99a8e181c
Fraditya <ruuoreo.o@gmail.com>
2023-12-14 15:20:00 +0700

    intial commit

---
+++ eas-nuxt/pages/SubmissionStatus/[email].vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        This email: {{ accept.email }} status is {{ accept.status }}
+    </div>
+</template>
+<script setup>
+const route = useRoute().params.email
+const { data: request } = await useFetch('http://localhost:5000/api/submissions');
+const submissions = request._rawValue.docs;
+
+let accept;
+
+submissions.forEach(submission => {
+    console.log(submission.email);
+    if (submission.email === route) {
+        accept = submission;
+    }
+});
+
+console.log(accept)
+</script>
---
+++ eas-nuxt/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <NuxtLink to="/submission"><button>Make a new Submission</button></NuxtLink>
+        <NuxtLink to="/submissionstatus"><button>Check Status</button></NuxtLink>
+    </div>
+</template>
+<script>
+</script>
+<style lang="">
+    
+</style>
